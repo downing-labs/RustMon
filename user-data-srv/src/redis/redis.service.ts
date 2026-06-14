7e36ef0bbd204db41ea28fd355766f20d5318d30
Hackpig1974 <me@damon1974.com>
2026-06-14 11:40:23 -0600

    Upgrade NestJS 8->10, replace redis v3 with ioredis v5, add axios explicitly

--- user-data-srv/src/redis/redis.service.ts
+++ user-data-srv/src/redis/redis.service.ts
@@ -1,98 +1,55 @@
-import { Injectable, Logger } from '@nestjs/common';
-import * as redis from 'redis';
-import { Subject } from 'rxjs';
+import { Injectable, Logger, OnModuleDestroy } from '@nestjs/common';
+import Redis from 'ioredis';
 import { environment } from 'src/environment';
-
-const crypto = require('crypto');
+import { createHash } from 'crypto';
 
 @Injectable()
-export class CacheRedisService {
+export class CacheRedisService implements OnModuleDestroy {
 
     private readonly logger = new Logger(CacheRedisService.name);
-
-    private readonly redisClient: redis.RedisClient;
-    private onError: Subject<any> = new Subject<any>();
+    private readonly redisClient: Redis;
 
     constructor() {
-        this.redisClient = redis.createClient({
+        this.redisClient = new Redis({
             host: environment.redis.host,
-            auth_pass: environment.redis.auth_pass,
-            port: environment.redis.port
+            port: environment.redis.port,
+            password: environment.redis.auth_pass,
         });
         this.redisClient.on('error', (err) => {
             this.logger.error('Redis cache error ' + err.toString());
-            this.onError.next(err);
         });
     }
-    
-    public getOnError(): Subject<any> {
-        return this.onError;
+
+    onModuleDestroy() {
+        this.redisClient.disconnect();
     }
 
-    /**
-     * 
-     * @param key the key to store in cache and recover
-     * @param ttl the time to live seconds (time in cache before auto-remove)
-     * @param data the data to save in cache
-     */
-    public saveInCache(key: string, ttl: number, data: string | object): boolean {
-        if(typeof data == 'object') {
-            data = JSON.stringify(data);
+    public async saveInCache(key: string, ttl: number, data: string | object): Promise<void> {
+        const value = typeof data === 'object' ? JSON.stringify(data) : data;
+        await this.redisClient.setex(key, ttl, value);
+    }
+
+    public async getFromCache(key: string, asObject?: boolean): Promise<any> {
+        const data = await this.redisClient.get(key);
+        if (!data) return false;
+        if (asObject) {
+            try {
+                return JSON.parse(data);
+            } catch (e) {}
         }
-        return this.redisClient.setex(key, ttl, data);
+        return data;
     }
 
-    /**
-     * @param key the key of value saved in cache
-     * @param asObject parse json or return string? if json is invalid, return the string
-     * @returns false, Object, String
-     */
-    public getFromCache(key: string, asObject?: boolean): Promise<any> {
-        return new Promise<any>((res, rej) => {
-            this.redisClient.get(key, (err, data) => {
-                if(err) {
-                    rej(err);
-                } else {
-                    if(data) {
-                        if(asObject) {
-                            try {
-                                const _data = JSON.parse(data);
-                                if(_data) {
-                                    data = _data;
-                                }
-                            } catch(e) {}
-                        }
-                        res(data);
-                    } else {
-                        res(false);
-                    }
-                }
-            });
-        });
+    public async invalidate(key: string): Promise<boolean> {
+        const result = await this.redisClient.del(key);
+        return result > 0;
     }
 
-    /**
-     * 
-     * @param key delete key and value from cache.
-     */
-    public invalidate(key: string) {
-        return this.redisClient.del(key);
+    public checksum(data: string): string {
+        return createHash('md5').update(data).digest('hex');
     }
 
-    /**
-     * create hash md5 of data to generate key based on values:
-     * @param data data to checksum
-     */
-    public checksum(data: string) {
-        return crypto.createHash('md5').update(data).digest('hex').toString();
+    public async flushAll(): Promise<void> {
+        await this.redisClient.flushall();
     }
-
-    /**
-     * 
-     * @param key clear kvs.
-     */
-    public flushAll() {
-        return this.redisClient.flushall();
-    }
-
 }
